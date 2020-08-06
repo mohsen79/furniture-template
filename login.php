655ab46cfd914a58fdbb19eabd617cbe7e9c72d7
mohsen79 <mohsenjalali.developer@gmail.com>
2020-08-06 16:15:16 +0430

    show has been finished

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<haed>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+</haed>
+<body>
+
+    <div class="sign-in">
+        <input type="text" name="username" placeholder="username" id="username">
+        <input type="password" name="pas" placeholder="password" id="password">
+        <a href="main.php"><input type="button" id="back" value="HOME"></a>
+        <input type="button" id="enter" value="sign in">
+    </div>
+
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="responsive.css">
+</head>
+    <body>
+
+        <div class="img-back">
+            <img src="img/fade1.jpg">
+            <div class="show-text"><p></p></div>
+            <a href="main.php"><input type="button" value="BACK" class="back"></a>
+        </div>
+
+
+    </body>
+</html>
