efff20385295adea0a37ad457a8eebf453a0b0dd
mohsen79 <mohsenjalali.developer@gmail.com>
2020-08-18 19:09:50 +0430

    time menu finished

--- backend.php
+++ backend.php
@@ -1,140 +1,232 @@
 <?php
 
-    include_once "connection.php" ;
-    // main menu
-    if(isset($_POST["ok"])){
-        if(empty($_POST["title"])){
-            header("location:edit.php?empty=0");
+include_once "connection.php";
+// main menu
+if (isset($_POST["ok"])) {
+    if (empty($_POST["title"])) {
+        header("location:edit.php?empty=0");
+        exit;
+    } else {
+        $title = $_POST["title"];
+        $ins = "INSERT INTO `main menu` (title) VALUES ('$title')";
+        $result = $connect->prepare($ins);
+        $result->execute();
+        if ($result) {
+            header("location:edit.php?ok=1");
             exit;
-        } else{
-            $title = $_POST["title"];
-            $ins = "INSERT INTO `main menu` (title) VALUES ('$title')";
-            $result = $connect->prepare($ins);
-            $result->execute();
-            if($result){
-                header("location:edit.php?ok=1");
-                exit;
-            } else{
-                header("location:edit.php?error=2");
-                exit;
-            }
-        }
-    }
-
-    // main photo send
-    if(isset($_POST["send-photo"])){
-        if(empty($_FILES["pic-input"])){
-            header("location:edit.php?empty2=3");
-           exit;
-        }
-        else{
-            $filename = $_FILES["pic-input"]["name"];
-            $filetemp = $_FILES["pic-input"]["tmp_name"];
-            if(is_uploaded_file($filetemp)){
-                if(move_uploaded_file($filetemp,$filename)){
-                     echo "file has been uploaded successfully<br>";
-                    echo "<a href='".$filename."'> file address </a>";
-                }
-                else{
-                    header("location:edit.php?error2=5");
-                    exit;
-                }
-            } 
-        }
-
-    }
-    //main photo upload
-    if(isset($_POST["upload-btn"])){
-        if(empty($_POST["main-pic"])){
-            header("location:edit.php?empty3=7");   
-            exit;
-        } 
-        else{
-            $src = $_POST["main-pic"];
-            $ins = "INSERT INTO `main picture` (src) VALUES ('$src')";
-            $result = $connect->prepare($ins);
-            $result->execute();
-        }
-        if($result){
-            header("location:edit.php?ok3=8");
-            exit;
-        } else{
-            header("location:edit.php?error3=9");
+        } else {
+            header("location:edit.php?error=2");
             exit;
         }
     }
+}
 
-    /* fade menu section 1 */
-    if(isset($_POST["fade-btn1"])){
-        if(empty($_POST["fade-title1"]) || empty($_POST["fade-con1"]) || empty($_POST["fade-src1"])){
-            header("location:edit.php?empty4=10");
-            exit;
-        }
-        else{
-            $title = $_POST["fade-title1"];
-            $content = $_POST["fade-con1"];
-            $src = $_POST["fade-src1"];
-            $ins = "INSERT INTO `fade menu1` (title , content , src) VALUES ('$title' , '$content' , '$src')";
-            $result = $connect->prepare($ins);
-            $result->execute();
-            if($result){
-                header("location:edit.php?ok4=1");
-                exit;
-            } 
-            else{
-                header("location:edit.php?error4=1");
+// main photo send
+if (isset($_POST["send-photo"])) {
+    if (empty($_FILES["pic-input"])) {
+        header("location:edit.php?empty2=3");
+        exit;
+    } else {
+        $filename = $_FILES["pic-input"]["name"];
+        $filetemp = $_FILES["pic-input"]["tmp_name"];
+        if (is_uploaded_file($filetemp)) {
+            if (move_uploaded_file($filetemp, $filename)) {
+                // "file has been uploaded successfully<br>";
+                //  echo "<a href='".$filename."'> file address </a>";
+                $address=(stream_resolve_include_path($filename));
+               echo "<script>alert('the photo address : ".$address."')</script>";
+            } else {
+                header("location:edit.php?error2=5");
                 exit;
             }
         }
     }
+}
+//main photo upload
+if (isset($_POST["upload-btn"])) {
+    if (empty($_POST["main-pic"])) {
+        header("location:edit.php?empty3=7");
+        exit;
+    } else {
+        $src = $_POST["main-pic"];
+        $ins = "INSERT INTO `main picture` (src) VALUES ('$src')";
+        $result = $connect->prepare($ins);
+        $result->execute();
+    }
+    if ($result) {
+        header("location:edit.php?ok3=8");
+        exit;
+    } else {
+        header("location:edit.php?error3=9");
+        exit;
+    }
+}
 
-    /* fade menu section 2 */
-    if(isset($_POST["fade-btn2"])){
-        if(empty($_POST["fade-title2"]) || empty($_POST["fade-con2"]) || empty($_POST["fade-src2"])){
-            header("location:edit.php?empty5=10");
-            exit;
-        }
-        else{
-            $title = $_POST["fade-title2"];
-            $content = $_POST["fade-con2"];
-            $src = $_POST["fade-src2"];
-            $ins = "INSERT INTO `fade menu2` (title , content , src) VALUES ('$title' , '$content' , '$src')";
-            $result = $connect->prepare($ins);
-            $result->execute();
-            if($result){
-                header("location:edit.php?ok5=1");
-                exit;
-            } 
-            else{
-                header("location:edit.php?error5=1");
+/* send photo fade menu section 1 */
+if (isset($_POST["fade-pic-btn"])) {
+    if (empty($_FILES["fade-pic"])) {
+        header("location:edit.php?empty12=3");
+        exit;
+    } else {
+        $filename2 = $_FILES["fade-pic"]["name"];
+        $filetemp2 = $_FILES["fade-pic"]["tmp_name"];
+        if (is_uploaded_file($filetemp2)) {
+            if (move_uploaded_file($filetemp2, $filename2)) {
+               $address=(stream_resolve_include_path($filename2));
+               echo "<script>alert('the photo address : ".$address."')</script>";
+               header("refresh:0");
+            } else {
+                header("location:edit.php?error7=5");
                 exit;
             }
         }
     }
+}
 
-    /*send slider picture */
-    if(isset($_POST["send-photo2"])){
-        if(empty($_FILES["pic-input2"])){
-            header("location:edit.php?empty6=3");
-           exit;
-        }
-        else{
-            $filename = $_FILES["pic-input2"]["name"];
-            $filetemp = $_FILES["pic-input2"]["tmp_name"];
-            if(is_uploaded_file($filetemp)){
-                if(move_uploaded_file($filetemp,$filename)){
-                     //echo "file has been uploaded successfully<br>";
-                     //print "<a href='".$filename."'> file address </a>";
-                     header("location:edit.php?ok6=$filename");
-                     echo "edit.php?ok6=$filename";
-                }
-                else{
-                    header("location:edit.php?error6=5");
-                    exit;
-                }
-            } 
+/* fade menu section 1 */
+if (isset($_POST["fade-btn1"])) {
+    if (empty($_POST["fade-title1"]) || empty($_POST["fade-con1"]) || empty($_POST["fade-src1"])) {
+        header("location:edit.php?empty4=10");
+        exit;
+    } else {
+        $title = $_POST["fade-title1"];
+        $content = $_POST["fade-con1"];
+        $src = $_POST["fade-src1"];
+        $ins = "INSERT INTO `fade menu1` (title , content , src) VALUES ('$title' , '$content' , '$src')";
+        $result = $connect->prepare($ins);
+        $result->execute();
+        if ($result) {
+            header("location:edit.php?ok4=1");
+            exit;
+        } else {
+            header("location:edit.php?error4=1");
+            exit;
         }
     }
+}
 
+/* send photo fade menu section 2 */
+if (isset($_POST["fade-pic-btn2"])) {
+    if (empty($_FILES["fade-pic2"])) {
+        header("location:edit.php?empty13=3");
+        exit;
+    } else {
+        $filename2 = $_FILES["fade-pic2"]["name"];
+        $filetemp2 = $_FILES["fade-pic2"]["tmp_name"];
+        if (is_uploaded_file($filetemp2)) {
+            if (move_uploaded_file($filetemp2, $filename2)) {
+               $address=(stream_resolve_include_path($filename2));
+               echo "<script>alert('the photo address : ".$address."')</script>";
+               header("refresh:0");
+            } else {
+                header("location:edit.php?error7=5");
+                exit;
+            }
+        }
+    }
+}
 
-?>
+/* fade menu section 2 */
+if (isset($_POST["fade-btn2"])) {
+    if (empty($_POST["fade-title2"]) || empty($_POST["fade-con2"]) || empty($_POST["fade-src2"])) {
+        header("location:edit.php?empty5=10");
+        exit;
+    } else {
+        $title = $_POST["fade-title2"];
+        $content = $_POST["fade-con2"];
+        $src = $_POST["fade-src2"];
+        $ins = "INSERT INTO `fade menu2` (title , content , src) VALUES ('$title' , '$content' , '$src')";
+        $result = $connect->prepare($ins);
+        $result->execute();
+        if ($result) {
+            header("location:edit.php?ok5=1");
+            exit;
+        } else {
+            header("location:edit.php?error5=1");
+            exit;
+        }
+    }
+}
+
+/*send slider picture */
+if (isset($_POST["send-photo2"])) {
+    if (empty($_FILES["pic-input2"])) {
+        header("location:edit.php?empty6=3");
+        exit;
+    } else {
+        $filename2 = $_FILES["pic-input2"]["name"];
+        $filetemp2 = $_FILES["pic-input2"]["tmp_name"];
+        if (is_uploaded_file($filetemp2)) {
+            if (move_uploaded_file($filetemp2, $filename2)) {
+               $address=(stream_resolve_include_path($filename2));
+               echo "<script>alert('the photo address : ".$address."')</script>";
+               header("refresh:0");
+            } else {
+                header("location:edit.php?error6=5");
+                exit;
+            }
+        }
+    }
+}
+
+/* upload slider picture */
+if (isset($_POST["upload-btn2"])) {
+    if (empty($_POST["slider-pic"])) {
+        header("location:edit.php?empty14=7");
+        exit;
+    } else {
+        $src = $_POST["slider-pic"];
+        $ins = "INSERT INTO `slider` (src) VALUES ('$src')";
+        $result = $connect->prepare($ins);
+        $result->execute();
+    }
+    if ($result) {
+        header("location:edit.php?ok14=8");
+        exit;
+    } else {
+        header("location:edit.php?error14=9");
+        exit;
+    }
+}
+
+/* send menu time photo */
+if (isset($_POST["send-btn3"])) {
+    if (empty($_FILES["time-input3"])) {
+        header("location:edit.php?empty15=3");
+        exit;
+    } else {
+        $filename2 = $_FILES["time-input3"]["name"];
+        $filetemp2 = $_FILES["time-input3"]["tmp_name"];
+        if (is_uploaded_file($filetemp2)) {
+            if (move_uploaded_file($filetemp2, $filename2)) {
+               $address=(stream_resolve_include_path($filename2));
+               echo "<script>alert('the photo address : ".$address."')</script>";
+               header("refresh:0");
+            } else {
+                header("location:edit.php?error15=5");
+                exit;
+            }
+        }
+    }
+}
+
+/* upload time menu picture */
+
+if (isset($_POST["upload-btn3"])) {
+    if (empty($_POST["time-src"])) {
+        header("location:edit.php?empty16=7");
+        exit;
+    } else {
+        $src = $_POST["time-src"];
+        $ins = "INSERT INTO `menu time` (src) VALUES ('$src')";
+        $result = $connect->prepare($ins);
+        $result->execute();
+    }
+    if ($result) {
+        header("location:edit.php?ok16=8");
+        exit;
+    } else {
+        header("location:edit.php?error16=9");
+        exit;
+    }
+}
--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="responsive.css">
 </head>
-
+<?php include "backend.php"; ?>
 <body>
 
     <h2 class="edit-title">EDIT PAGE</h2>
@@ -108,15 +108,14 @@
             title :&nbsp&nbsp&nbsp<input type="text" id="edit-main-menu" name="title"><input type="submit" name="ok" id="title-ok" value="ok">
         </form>
         <?php
-        //include "backend.php";
         if (isset($_GET["empty"])) {
             echo "<font color=red>fill the title</font>";
         }
         if (isset($_GET["ok"])) {
-            echo "change has done successfully";
+            echo "<font color=green>ok</font>";
         }
         if (isset($_GET["error"])) {
-            echo "ERROR";
+            echo "<font color=red>ERROR</font>";
         }
         ?>
     </fieldset>
@@ -124,26 +123,26 @@
         <legend>edit main picture</legend>
         <form action="" method="post" enctype="multipart/form-data">
             select a photo :&nbsp&nbsp&nbsp<input type="file" id="edit-main-pic" name="pic-input">
-            <input type="submit" name="send-photo" id="send-photo" value="send">
+            <input type="submit" name="send-photo" id="send-photo" value="phtot address">
             <br><br>
             src : &nbsp&nbsp <input type="text" id="edit-main-upload" name="main-pic"><input type="submit" name="upload-btn" id="upload-btn" value="upload">
         </form>
         <?php
-        require "backend.php";
+        //require "backend.php";
         if (isset($_GET["empty2"])) {
-            echo "empty";
+            echo "<font color=red>empty</font>";
         }
         if (isset($_GET["error2"])) {
-            echo "ERROR";
+            echo "<font color=red>ERROR</font>";
         }
         if (isset($_GET["empty3"])) {
-            echo "empty";
+            echo "<font color=red>empty</font>";
         }
         if (isset($_GET["ok3"])) {
-            echo "right";
+            echo "<font color=green>ok</font>";
         }
         if (isset($_GET["error3"])) {
-            echo "ERROR";
+            echo "<font color=red>ERROR</font>";
         }
 
         ?>
@@ -163,45 +162,47 @@
 
 
         ?>
-            <form action="" method="post">
+            <form action="" method="post" enctype="multipart/form-data">
                 title :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" id="edit-fademenu-title" name="fade-title1"><br><br><br>
                 content : &nbsp&nbsp&nbsp <input type="text" id="edit-fademenu-content" name="fade-con1">
                 <br><br><br>
+                select a photo :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input type="file" name="fade-pic"><br>
                 src : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" id="fade-menu-src" name="fade-src1"><br><br><br>
-                <input type="submit" id="fade-menu-ok" value="ok" name="fade-btn1">
+                <input type="submit" id="fade-menu-ok" value="ok" name="fade-btn1"><input type="submit" name="fade-pic-btn" id="fade-pic" value="photo adress">
             </form>
         <?php
         //}
         if (isset($_GET["empty4"])) {
-            echo "empty";
+            echo "<font color=red>empty</font>";
         }
         if (isset($_GET["ok4"])) {
-            echo "ok";
+            echo  "<font color=green>ok</font>";
         }
         if (isset($_GET["error4"])) {
-            echo "error";
+            echo "<font color=red>ERROR</font>";
         }
 
         ?>
     </fieldset>
     <fieldset>
         <legend>edit main fade menu section 2</legend>
-        <form action="" method="post">
+        <form action="" method="post" enctype="multipart/form-data">
             title :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" id="edit-fademenu-title2" name="fade-title2"><br><br><br>
             content : &nbsp&nbsp&nbsp <input type="text" id="edit-fademenu-content2" name="fade-con2"><br><br><br>
+            select a photo :&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="file" name="fade-pic2"><br>
             src : &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" id="fade-menu-src2" name="fade-src2"><br><br><br>
-            <input type="submit" id="fade-menu-ok2" value="ok" name="fade-btn2">
+            <input type="submit" id="fade-menu-ok2" value="ok" name="fade-btn2"><input type="submit" name="fade-pic-btn2" id="fade-pic" value="photo address">
         </form>
         <?php
         //}
         if (isset($_GET["empty5"])) {
-            echo "empty";
+            echo "<font color=red>empty</font>";
         }
         if (isset($_GET["ok5"])) {
-            echo "ok";
+            echo "<font color=green>ok</font>";
         }
         if (isset($_GET["error5"])) {
-            echo "error";
+            echo "<font color=red>ERROR</font>";
         }
 
         ?>
@@ -210,35 +211,64 @@
         <legend>edit slider pictures</legend>
         <form action="" method="post" enctype="multipart/form-data">
             select a photo :&nbsp&nbsp&nbsp<input type="file" id="edit-slider-pic" name="pic-input2">
-            <input type="submit" name="send-photo2" id="send-photo" value="send">
+            <input type="submit" name="send-photo2" id="send-photo" value="phtot address">
             <br><br>
-            src : &nbsp&nbsp <input type="text" id="edit-slider-upload"><input type="button" id="upload-btn2" value="upload">
+            src : &nbsp&nbsp <input type="text" id="edit-slider-upload" name="slider-pic"><input type="submit" id="upload-btn2" value="upload" name="upload-btn2">
         </form>
         <?php
-        //}
-        require "backend.php";
+       // include "backend.php";
         if (isset($_GET["empty6"])) {
-            echo "empty";
+            echo "<font color=red>empty</font>";
         }
         if (isset($_GET["ok6"])) {
-            echo "ok";
-            global $filename;
-            $address = "<a href='".$filename."'> file address </a>";
-            echo $address;
-           // echo "<a href='".$filename."'> file address </a>";
+            echo "<font color=green>ok</font>";
         }
         if (isset($_GET["error6"])) {
-            echo "error";
+            echo "<font color=red>ERROR</font>";
         }
+        if (isset($_GET["empty14"])) {
+            echo "<font color=red>empty</font>";
+        }
+        if (isset($_GET["ok14"])) {
+            echo "<font color=green>ok</font>";
+        }
+        if (isset($_GET["error14"])) {
+            echo "<font color=red>ERROR</font>";
+        }
+
 
         ?>
     </fieldset>
     <fieldset>
         <legend>edit menu time pictures</legend>
-        <form action="" method="post">
-            select a photo :&nbsp&nbsp&nbsp<input type="file" id="edit-mneu-time-pic"><br><br>
-            src : &nbsp&nbsp <input type="text" id="edit-menu-time-upload"><input type="button" id="upload-btn3" value="upload">
+        <form action="" method="post" enctype="multipart/form-data">
+            select a photo :&nbsp&nbsp&nbsp<input type="file" id="edit-mneu-time-pic" name="time-input3">
+            <input type="submit" name="send-btn3" value="photo address" id="send-photo3">
+            <br><br>
+            src : &nbsp&nbsp <input type="text" id="edit-menu-time-upload" name="time-src"><input type="submit" id="upload-btn3" value="upload" name=upload-btn3>
         </form>
+        <?php
+        if (isset($_GET["empty15"])) {
+            echo "<font color=red>empty</font>";
+        }
+        if (isset($_GET["ok15"])) {
+            echo "<font color=green>ok</font>";
+        }
+        if (isset($_GET["error15"])) {
+            echo "<font color=red>ERROR</font>";
+        }
+        if (isset($_GET["empty16"])) {
+            echo "<font color=red>empty</font>";
+        }
+        if (isset($_GET["ok16"])) {
+            echo "<font color=green>ok</font>";
+        }
+        if (isset($_GET["error16"])) {
+            echo "<font color=red>ERROR</font>";
+        }
+
+
+        ?>
     </fieldset>
 
 </body>
